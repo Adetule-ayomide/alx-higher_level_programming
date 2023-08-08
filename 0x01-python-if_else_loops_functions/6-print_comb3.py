480035c64cd1abcf9b07ca9d9afed40512908a35
Adetule-ayomide <adetuleayomide@gmail.com>
2023-08-08 16:11:38 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
+for digit_one in range(0, 10):
+    for digit_two in range(digit_one + 1, 10):
+        if digit_one == 8 and digit_two == 9:
+            print("{}{}".format(digit_one, digit_two))
         else:
-            print("{}{}".format(digit1, digit2), end=", ")
+            print("{}{}".format(digit_one, digit_two), end=", ")
