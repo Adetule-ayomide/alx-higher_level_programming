c1ecc888ccb85dad76b00901d940e9f5a2ca2073
Adetule-ayomide <adetuleayomide@gmail.com>
2023-08-09 14:33:55 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for letter in range(26):
     if(letter % 2 == 0):
         print(chr(122 - letter), end="")
     else:
-        print(chr(90 - letter),end="")
+        print(chr(90 - letter), end="")
