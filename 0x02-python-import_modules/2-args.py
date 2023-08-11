1b67ebf45c8f694d8bbb4a6a1fe7a80177eb9c9f
Adetule-ayomide <adetuleayomide@gmail.com>
2023-08-10 18:38:02 -0700

    a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 from sys import argv
 
+
 def main():
     if len(argv) == 1:
         print("{:d} arguments{}".format(len(argv) - 1, '.'))
         return
-    
+    elif len(argv) == 2:
+        print("{} argument{}:".format(len(argv) - 1, ""))
     else:
-        print("{} argument{}:".format(len(argv) - 1, "" if len(argv) == 2 else "s"))
-    
+        print("{} argument{}:".format(len(argv) - 1, "s"))
+
     if(len(argv) >= 2):
         for counter, argument in enumerate(argv[1:], start=1):
             print("{}: {}".format(counter, argument))
 
+
 if __name__ == "__main__":
-    main()       
+    main()
