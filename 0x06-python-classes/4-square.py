28b144d3cfbf8a2774176b02ae5c981b3a0ad2d0
adetule-ayomide <adetuleayomide@gmail.com>
2023-08-30 00:16:32 +0100

    a class Square that defines a square by: (based on 3-square.py)

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,10 +14,12 @@ class Square:
         """
         self.__size = size
 
+    @property
     def size(self):
          "To retrieve the size"
          return self.__size
 
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
