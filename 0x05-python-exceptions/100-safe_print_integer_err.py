04c0f1342e8c632ce4b453c5c21a870a110347d7
adetule-ayomide <adetuleayomide@gmail.com>
2023-08-29 04:59:30 +0100

    a function that prints an integer.

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as k:
+        print("Exception: {}".format(k), file=sys.stderr)
+        return False
+
