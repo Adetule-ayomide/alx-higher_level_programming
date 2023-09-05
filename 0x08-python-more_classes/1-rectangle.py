953d07df0679bc102e9c0af04edf75f149cb1cb0
Adetule-ayomide <adetuleayomide@gmail.com>
2023-09-04 20:47:50 -0700

    a class Rectangle that defines a rectangle by: (based on 0-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,23 +3,34 @@
 
 
 class Rectangle:
-    """ a class Rectangle that defines a rectangle by:
-        (based on 0-rectangle.py)
-        @width: width of the rectangle
-        @height: height of the rectangle
-    """
+    """A class Rectangle that defines a rectangle"""
+
     def __init__(self, width=0, height=0):
+        """
+        Args:
+            width: width of a rectangle
+            height: height of a rectangle
+        """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """getter function: a function that returns width"""
+        """property that retrieves the rectangle's width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """setter function: a function that set the value"""
+        """
+        property that sets the rectangle's width
+        Args:
+            value: the given value
+        Returns:
+            the width
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than 0
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -28,12 +39,21 @@ class Rectangle:
 
     @property
     def height(self):
-        """getter function: a function that returns height"""
+        """property that sets the rectangle's height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ setter function: a function that set the value"""
+        """
+        property that sets the rectangle's height
+        Args:
+            value: the given value
+        Returns:
+            the height
+        Raises:
+            TypeError: if value isn't integer
+            ValueError: if value is less than 0
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
