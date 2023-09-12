916f763692e212810f8a0f606fa35678a40462f2
Adetule-ayomide <adetuleayomide@gmail.com>
2023-09-11 17:13:13 -0700

    a function that returns the list of available attributes and methods of an object:

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""a function that returns the list of available
+    attributes and methods of an object:
+
+    Prototype: def lookup(obj):
+    Returns a list object
+"""
+
+
+def lookup(obj):
+    attributes_and_methods = dir(obj)
+    return attributes_and_methods
