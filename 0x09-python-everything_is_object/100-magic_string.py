b841bf1ee1dfb22ec88678a81c12447afa49ce4a
Adetule-ayomide <adetuleayomide@gmail.com>
2023-09-11 19:08:48 -0700

    function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from,

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def magic_string():
-    string += “BestSchool”
-    return string
+def magic_string(n=1):
+    string = "BestSchool"
+    return ", ".join([string =+ string (", " if i < n else "$") for i in range(n)])
+
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+""" a function that returns True if the object is an instance of,
+    or if the object is an instance of a class that
+    inherited from, the specified class ; otherwise False.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """A function that return True or False
+
+    Args:
+        @obj: object to check
+        @a_class: instances of a class
+    """
+    if isinstance(obj, a_class)
+        return True
+    else:
+        return False
