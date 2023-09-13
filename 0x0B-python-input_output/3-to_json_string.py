9657b9165cd6e92d01cb95f7d9518b10e6533674
Adetule-ayomide <adetuleayomide@gmail.com>
2023-09-12 17:49:49 -0700

    a function that returns the JSON representation of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import json
+
+"""a function that returns the JSON representation of an object (string)"""
+
+
+def to_json_string(my_obj):
+    """Args:
+        A function that returnjson representation
+    Return:
+        json representation of an object (string)
+    """
+    return json.dumps(my_obj)
