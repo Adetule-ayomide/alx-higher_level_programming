4ac20713e9e6fd3faebd9212dbeb347fbea6a8c3
Adetule-ayomide <adetuleayomide@gmail.com>
2023-09-12 17:15:10 -0700

    a function that appends a string at the end of a text file (UTF8) and returns the number of characters added

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""a function that appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """function that appends a string at the end of a text file"""
+    with open(filename, "a", encoding="utf-8") as file
+    file.write(text)
+    char_num = len(file)
+    return (char_num)
