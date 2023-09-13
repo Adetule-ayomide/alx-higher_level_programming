12bc188f0dde40fa9396868044ab461335ccd47a
Adetule-ayomide <adetuleayomide@gmail.com>
2023-09-13 15:22:49 -0700

    a class Student that defines a student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -15,6 +15,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """A function that retrieves a dictionary representation"""
         return self.__dict__
