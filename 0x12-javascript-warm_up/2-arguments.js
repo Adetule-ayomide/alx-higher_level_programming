7c82c1621cc3d6c928778b808386060ac434dcb1
Adetule-ayomide <adetuleayomide@gmail.com>
2023-11-13 12:32:11 -0800

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const {argv} = require('node:process');
+const argv = process.argv.length;
 
-if (argv.length === 2){
+if (argv === 2){
 	console.log('No argument');
-} else if (argv.length === 3) {
+} else if (argv === 3) {
 	console.log('Argument found');
 } else {
 	console.log('Argument found');
