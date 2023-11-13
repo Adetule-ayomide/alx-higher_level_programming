aaaa4f7087324ff48e77fe744597d47211ff943d
Adetule-ayomide <adetuleayomide@gmail.com>
2023-11-13 15:58:07 -0800

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 const firstArg = Number(args[0]);
 
 if (args.length !== 1 || isNaN(firstArg)) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < firstArg; i++) {
     console.log('C is fun');
