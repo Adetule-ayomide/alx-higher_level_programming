35378f1cf4adb99cc68dce5683271313d8878000
Adetule-ayomide <adetuleayomide@gmail.com>
2023-11-17 11:22:50 -0800

    SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""a script that takes in an argument and displays all values in the
+    states table of hbtn_0e_0_usa where name matches the argument."""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """A function that display all values that match an argument passed"""
+
+    host = "localhost"
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db, port=port)
+    cursor = db.cursor()
+    cursor.execute("""
+                    SELECT * FROM states
+                    WHERE name LIKE %s
+                    ORDER BY id ASC""", (state_name,))
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
