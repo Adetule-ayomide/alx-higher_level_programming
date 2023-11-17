1f52c61daf032ab00938d8f1741fa7c88f5b6ebf
Adetule-ayomide <adetuleayomide@gmail.com>
2023-11-17 10:15:10 -0800

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""a script that lists all states with a name starting with N
+    (upper N) from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """A function that list states"""
+    host = "localhost"
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db, port=port)
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states
+                   WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
