764f98e6701d0e5b3e6a21e2ee663e9a6a7eba22
Adetule-ayomide <adetuleayomide@gmail.com>
2023-11-19 15:07:43 -0800

    List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-
-"""a script that lists all State objects, and corresponding City
-    objects, contained in the database hbtn_0e_101_usa"""
-
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from relationship_state import State
-from relationship_city import City
+"""a script that lists all State objects, and corresponding
+City objects, contained in the database
+hbtn_0e_101_usa"""
 
 
 def main():
-    """A function that list state"""
+    """list all realtionships"""
+    import sys
+    from relationship_state import State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
     host = "localhost"
     port = 3306
     user = sys.argv[1]
-    passwd = sys.argv[2]
+    pswd = sys.argv[2]
     db = sys.argv[3]
 
     engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(
@@ -23,10 +23,9 @@ def main():
 
     Session = sessionmaker(bind=engine)
     session = Session()
+    query = session.query(State).order_by(State.id)
 
-    states = session.query(State).order_by(State.id)
-
-    for state in states:
+    for state in query:
         print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
             print('\t{}: {}'.format(city.id, city.name))
