3b1b744006f0b369f4d8acbb2d70a42c6aa81f3c
Adetule-ayomide <adetuleayomide@gmail.com>
2023-12-01 14:21:12 -0800

    What's my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    utf8_content = body.decode('utf-8')
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
 
-print("Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body.decode('utf-8'))
-print("\t- utf8 content:", utf8_content)
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body.decode('utf-8'))
+    print("\t- utf8 content:", utf8_content)
