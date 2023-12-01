b4df7670b5f3c3b9b403a94cb1d555304d077487
Adetule-ayomide <adetuleayomide@gmail.com>
2023-11-30 18:35:23 -0800

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# a function that finds a peak in a list of unsorted integers.
+
+
+def find_peak(list_of_integers):
+    """A function that finds a peak"""
+    if len(list_of_integers) == 0:
+        return None
+
+    max_number = max(list_of_integers)
+    max_index = list_of_integers.index(max_number)
+
+    if len(list_of_integers) >= 2:
+        if max_index == 0 or max_index == len(list_of_integers) - 1:
+            return max_number
+
+    if len(list_of_integers) >= 3:
+        if list_of_integers[max_index] >= list_of_integers[max_index + 1] and list_of_integers[max_index] >= list_of_integers[max_index - 1]:
+            return max_number
