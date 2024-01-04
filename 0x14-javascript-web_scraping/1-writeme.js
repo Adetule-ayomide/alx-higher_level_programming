aad82b4198ee3a4f8671379eed4f299f5ff83754
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-03 21:02:19 -0800

    javascript

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const contentFile = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(contentFile, content, 'utf-8', err  => {
+	if (err) {
+		console.error(err);
+	}
+});
