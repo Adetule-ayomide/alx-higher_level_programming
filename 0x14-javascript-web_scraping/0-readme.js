98fc7d9f0e21f183c3d21347ed69c4cad48a95bb
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-04 18:54:19 -0800

    javascript web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,7 +7,7 @@ const filePath = process.argv[2];
 fs.readFile(filePath, 'utf-8', (err, data) => {
 	if (err) {
 		console.error(err);
-	} else {
-		console.log(data);
+		return;
 	}
+	console.log(data);
 });
