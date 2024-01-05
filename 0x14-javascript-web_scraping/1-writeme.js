b5907bf8110919aac5b17b42f2425f4fd0c6b1b0
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-04 19:13:57 -0800

    a script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,7 +6,8 @@ const contentFile = process.argv[2];
 const content = process.argv[3];
 
 fs.writeFile(contentFile, content, 'utf-8', err  => {
-    if (err) {
-        console.error(err);
-    }
+  if (err) {
+    console.error(err);
+  }
 });
+
