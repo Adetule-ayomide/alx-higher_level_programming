35b173940d6167dd912b2bbadd56c60b37d5ea14
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-04 19:26:53 -0800

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,6 @@ const request = require('request');
 const id = process.argv[2];
 
 request.get(`https://swapi-api.alx-tools.com/api/films/${id}`, function (response, body) {
-	const title = JSON.parse(body.body).title;
-	console.log(title);
+  const title = JSON.parse(body.body).title;
+  console.log(title);
 });
