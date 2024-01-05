4e0e5cb56134a01fc2ae3af2f76d0cb04c6db5be
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-04 19:31:45 -0800

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,11 +4,11 @@ const request = require('request');
 const address = process.argv[2];
 
 request.get(address, function (response, body) {
-	const result = JSON.parse(body.body).results;
-	let wedgeAntillesPresent = 0;
-	result.forEach(film => 
-		film.characters.forEach(character => 
-			if (character.endsWith('18/')) wedgeAntillesPresent += 1;
-		}));
-	console.log(wedgeAntillesPresent);
+  const result = JSON.parse(body.body).results;
+  let wedgeAntillesPresent = 0;
+  result.forEach(film =>
+    film.characters.forEach(character => {
+      if (character.endsWith('18/')) wedgeAntillesPresent += 1;
+    }));
+  console.log(wedgeAntillesPresent);
 });
